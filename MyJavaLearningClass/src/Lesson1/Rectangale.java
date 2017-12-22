055f90d0cd6defa3f3fc25762c13bc3b7d9c988c
Jahir <jahirul@divergenttechbd.com>
2017-12-22 21:48:02 +0600

    Rectangle Area Monira Assignment 2

--- MyJavaLearningClass/src/Lesson1/Rectangale.java
+++ MyJavaLearningClass/src/Lesson1/Rectangale.java
@@ -16,15 +16,15 @@ public class Rectangale {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		double totalArea;
+		double totalArea,rectangleHeight,rectangleWidth;
 		
 		Scanner scan=new Scanner(System.in);
 		System.out.println("Please Input Your Rectangle Height = ");
-		double rectangleHeight=scan.nextDouble();
+		rectangleHeight=scan.nextDouble();
 		
 		//Scanner scan1=new Scanner(System.in);
 		System.out.println("Please Input Your Rectangle Width = ");
-		double rectangleWidth=scan.nextDouble();
+		rectangleWidth=scan.nextDouble();
 		
 		totalArea=rectangleHeight*rectangleWidth;
 		
