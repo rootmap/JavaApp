e1b3eeaa32f9cc7c6abd935257e285b76f67dd21
Jahir <jahirul@divergenttechbd.com>
2017-12-22 21:58:47 +0600

    multi condition Operation  Using Console Learning Lesson 9

---
+++ MyJavaLearningClass/src/Lesson1/IfMultiCondition.java
@@ -0,0 +1,41 @@
+/**
+ * 
+ */
+package Lesson1;
+
+/**
+ * @author User
+ *
+ */
+public class IfMultiCondition {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub'
+		
+		int subjectOne=30;
+		int subjectTwo=50;
+		
+		if((subjectOne>=35) && (subjectTwo>=35)) // check all condition is true
+		{
+			System.out.println("Multi Condition statement is true.");
+		}
+		else
+		{
+			System.out.println("Multi Condition is false");;
+		}
+		
+		if((subjectOne>=35) || (subjectTwo>=35)) //check whatever one is true
+		{
+			System.out.println("Multi Condition statement is true.");
+		}
+		else
+		{
+			System.out.println("Multi Condition is false");;
+		}
+
+	}
+
+}
