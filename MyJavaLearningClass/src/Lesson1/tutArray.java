82c864039c6320e2ec4d94938be301c2044da0dd
Fahad Bhuyian <f.bhuyian@gmail.com>
2017-12-22 23:26:44 +0600

    array and print array using while loop Using Console Learning Lesson 13

---
+++ MyJavaLearningClass/src/Lesson1/tutArray.java
@@ -0,0 +1,31 @@
+/**
+ * 
+ */
+package Lesson1;
+
+/**
+ * @author User
+ *
+ */
+public class tutArray {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int[] myArray= {1,3,2,4,5};
+		//java array can't assign my types of value in one array 
+		//like it's not able to store int and decimal value in one aarray
+		
+		//System.out.println(myArray[0]); //induvidual array print in console
+		int i=0;
+		while(i<=4)
+		{
+			System.out.println(myArray[i]);
+			i++;
+		}
+
+	}
+
+}
