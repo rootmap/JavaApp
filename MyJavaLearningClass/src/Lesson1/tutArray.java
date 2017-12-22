58dfa5b827f5251991758cacf43eb706c6079188
Fahad Bhuyian <f.bhuyian@gmail.com>
2017-12-22 23:34:35 +0600

    array print using while,for loop & element for loop Using Console Learning Lesson 14

--- MyJavaLearningClass/src/Lesson1/tutArray.java
+++ MyJavaLearningClass/src/Lesson1/tutArray.java
@@ -14,16 +14,29 @@ public class tutArray {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		int[] myArray= {1,3,2,4,5};
+		int[] myArray= {1,3,2,4,5}; // this is our array
 		//java array can't assign my types of value in one array 
 		//like it's not able to store int and decimal value in one aarray
 		
 		//System.out.println(myArray[0]); //induvidual array print in console
+		System.out.println("------------------while---loop---------------");
 		int i=0;
 		while(i<=4)
 		{ //without count we have to count manually how many array quantity we have
 			System.out.println(myArray[i]);
 			i++;
+		}
+		//print using for loop
+		System.out.println("------------------for---loop---------------");
+		for(i=0; i<5; i++)
+		{
+			System.out.println(myArray[i]);
+		}
+		//print using for loop element
+		System.out.println("---------------for loop using element------------------");
+		for(int element : myArray)
+		{
+			System.out.println(element);
 		}
 
 	}
