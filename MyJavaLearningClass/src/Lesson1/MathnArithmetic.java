a16ddce28c0fa0ce8f909ca7f7b5f35a2a75a9ba
Jahir <jahirul@divergenttechbd.com>
2017-12-22 20:53:21 +0600

    Math & Arithmetic Using Console Learning Lesson 6

---
+++ MyJavaLearningClass/src/Lesson1/MathnArithmetic.java
@@ -0,0 +1,32 @@
+/**
+ * 
+ */
+package Lesson1;
+
+/**
+ * @author User
+ *
+ */
+public class MathnArithmetic {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		double x,y,answer;
+		x=70;
+		y=30;
+		answer=x+y; 
+		//answer=x-y; 
+		//answer=x*y; 
+		//answer=x/y; 
+		//answer=x%y; 
+		// + - * / % (% Sign name is modulas)
+		
+		System.out.println("Your Answer is = "+answer);
+		
+
+	}
+
+}
