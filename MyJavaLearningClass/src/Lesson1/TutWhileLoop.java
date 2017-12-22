7cb647c1648aa9c164cb49dbde748fc4180f6044
Fahad Bhuyian <f.bhuyian@gmail.com>
2017-12-22 22:26:36 +0600

    While Loop Using Console Learning Lesson 11

---
+++ MyJavaLearningClass/src/Lesson1/TutWhileLoop.java
@@ -0,0 +1,32 @@
+/**
+ * 
+ */
+package Lesson1;
+
+/**
+ * @author User
+ *
+ */
+public class TutWhileLoop {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int i=0;
+		while(i<=10) //asc format
+		{
+			System.out.println(i);
+			i++;
+		}
+		System.out.println("----------------------------------------");
+		while(i>=0) //DESC format
+		{
+			System.out.println(i);
+			i--;
+		}
+
+	}
+
+}
