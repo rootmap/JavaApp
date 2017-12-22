c5b724898a24d526c3685ae9af824e39654991f8
Jahir <jahirul@divergenttechbd.com>
2017-12-22 21:22:18 +0600

    Incremental Operational  Using Console Learning Lesson 7

---
+++ MyJavaLearningClass/src/Lesson1/IOAO.java
@@ -0,0 +1,34 @@
+/**
+ * 
+ */
+package Lesson1;
+
+/**
+ * @author User
+ *
+ */
+public class IOAO {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int x=10;
+		x+=5;
+		System.out.println("Incremented Value Is = "+x);
+		x++;
+		System.out.println("Incremented Value Is = "+x);
+		x-=5;
+		System.out.println("Incremented Value Is = "+x);
+		x--;
+		System.out.println("Incremented Value Is = "+x);
+		x*=5;
+		System.out.println("Incremented Value Is = "+x);
+		x/=5;
+		System.out.println("Incremented Value Is = "+x);
+
+	}
+
+}
